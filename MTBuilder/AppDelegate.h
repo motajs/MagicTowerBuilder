7445e8099b3a659caa38d86ad34b801715e327c3
熊典 <x@xiongdianpku.com>
2018-05-26 15:50:26 +0800

    Initial Commit

---
+++ MTBuilder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MTBuilder
+//
+//  Created by 熊典 on 2018/5/26.
+//  Copyright © 2018年 熊典. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MTBuilder/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MTBuilder
+//
+//  Created by 熊典 on 2018/5/26.
+//  Copyright © 2018年 熊典. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
